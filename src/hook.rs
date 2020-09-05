410a1844623e98599d30682d13e22c6144e48c34
Inseok Lee <dlunch@gmail.com>
2020-09-05 15:22:47 +0900

    Log filename

--- src/hook.rs
+++ src/hook.rs
@@ -1,12 +1,14 @@
 use detour::GenericDetour;
 use dlopen::raw::Library;
+use log::debug;
+use widestring::WideCStr;
 
-type FnCreateFileW = extern "stdcall" fn(u64, u32, u32, u64, u32, u32, u64) -> u64;
+type FnCreateFileW = extern "stdcall" fn(*const u16, u32, u32, u64, u32, u32, u64) -> u64;
 
 static mut CREATE_FILE_W_HOOK: Option<GenericDetour<FnCreateFileW>> = None;
 
 extern "stdcall" fn hooked_create_file_w(
-    lp_file_name: u64,
+    lp_file_name: *const u16,
     dw_desired_access: u32,
     dw_share_mode: u32,
     lp_security_attributes: u64,
@@ -14,6 +16,9 @@ extern "stdcall" fn hooked_create_file_w(
     dw_flags_and_attributes: u32,
     h_template_file: u64,
 ) -> u64 {
+    let filename = unsafe { WideCStr::from_ptr_str(lp_file_name) };
+    debug!("{}", filename.to_string().unwrap());
+
     unsafe {
         CREATE_FILE_W_HOOK.as_ref().unwrap().call(
             lp_file_name,
--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,17 @@ mod wmvcore_wrapper;
 
 pub use wmvcore_wrapper::WMCreateReader;
 
+fn initialize() {
+    win_dbg_logger::init();
+
+    unsafe { hook::initialize_hook().unwrap() };
+}
+
 #[no_mangle]
 pub extern "stdcall" fn DllMain(_: u32, reason: u32, _: u64) -> u32 {
     if reason == 1 {
         // DLL_PROCESS_ATTACH
-        unsafe { hook::initialize_hook().unwrap() };
+        initialize()
     }
 
     1 // TRUE
