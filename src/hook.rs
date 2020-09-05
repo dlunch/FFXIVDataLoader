b27f1b92cf2b98d2dda5ebfaffc6917fc9947023
Inseok Lee <dlunch@gmail.com>
2020-09-05 14:52:16 +0900

    call original

--- src/hook.rs
+++ src/hook.rs
@@ -3,6 +3,8 @@ use dlopen::raw::Library;
 
 type FnCreateFileW = extern "stdcall" fn(u64, u32, u32, u64, u32, u32, u64) -> u64;
 
+static mut CREATE_FILE_W_HOOK: Option<GenericDetour<FnCreateFileW>> = None;
+
 extern "stdcall" fn hooked_create_file_w(
     lp_file_name: u64,
     dw_desired_access: u32,
@@ -12,16 +14,29 @@ extern "stdcall" fn hooked_create_file_w(
     dw_flags_and_attributes: u32,
     h_template_file: u64,
 ) -> u64 {
-    0
+    unsafe {
+        CREATE_FILE_W_HOOK.as_ref().unwrap().call(
+            lp_file_name,
+            dw_desired_access,
+            dw_share_mode,
+            lp_security_attributes,
+            dw_creation_disposition,
+            dw_flags_and_attributes,
+            h_template_file,
+        )
+    }
 }
 
 pub unsafe fn initialize_hook() -> Result<(), detour::Error> {
     let lib = Library::open("kernel32.dll").unwrap();
     let create_file = lib.symbol("CreateFileW").unwrap();
 
-    let hook = GenericDetour::<FnCreateFileW>::new(create_file, hooked_create_file_w)?;
+    CREATE_FILE_W_HOOK.replace(GenericDetour::<FnCreateFileW>::new(
+        create_file,
+        hooked_create_file_w,
+    )?);
 
-    hook.enable()?;
+    CREATE_FILE_W_HOOK.as_ref().unwrap().enable()?;
 
     Ok(())
 }
