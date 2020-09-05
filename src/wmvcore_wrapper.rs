2b3b745a94f6dbb94229ce2ceac56931ebb6a9ff
Inseok Lee <dlunch@gmail.com>
2020-09-05 14:48:48 +0900

    format

--- src/wmvcore_wrapper.rs
+++ src/wmvcore_wrapper.rs
@@ -1,6 +1,7 @@
 #![allow(non_snake_case)]
 
 use dlopen::wrapper::{Container, WrapperApi};
+
 #[derive(dlopen_derive::WrapperApi)]
 struct WmvCore {
     WMCreateReader: extern "stdcall" fn(p_unk_cert: u64, dw_rights: u64, pp_reader: u64) -> u64,
