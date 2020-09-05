541241e22081dbd8e178fd4f8f765fd83d9918ae
Inseok Lee <dlunch@gmail.com>
2020-09-05 14:48:23 +0900

    Remove unsafe

--- src/wmvcore_wrapper.rs
+++ src/wmvcore_wrapper.rs
@@ -3,18 +3,12 @@
 use dlopen::wrapper::{Container, WrapperApi};
 #[derive(dlopen_derive::WrapperApi)]
 struct WmvCore {
-    WMCreateReader:
-        unsafe extern "stdcall" fn(p_unk_cert: u64, dw_rights: u64, pp_reader: u64) -> u64,
+    WMCreateReader: extern "stdcall" fn(p_unk_cert: u64, dw_rights: u64, pp_reader: u64) -> u64,
 }
 
 #[no_mangle]
-#[allow(clippy::missing_safety_doc)]
-pub unsafe extern "stdcall" fn WMCreateReader(
-    p_unk_cert: u64,
-    dw_rights: u64,
-    pp_reader: u64,
-) -> u64 {
-    let cont: Container<WmvCore> = Container::load("wmvcore.dll").unwrap();
+pub extern "stdcall" fn WMCreateReader(p_unk_cert: u64, dw_rights: u64, pp_reader: u64) -> u64 {
+    let cont: Container<WmvCore> = unsafe { Container::load("wmvcore.dll").unwrap() };
 
     cont.WMCreateReader(p_unk_cert, dw_rights, pp_reader)
 }
