f6788130ce3068a20247497f31063be686001f6b
Inseok Lee <dlunch@gmail.com>
2020-09-19 08:20:06 +0900

    Hook ReadFile

--- src/sqpack_redirector.rs
+++ src/sqpack_redirector.rs
@@ -1,4 +1,9 @@
-use std::path::{Path, PathBuf};
+#![allow(unused_unsafe)] // clippy bug?
+
+use std::{
+    path::{Path, PathBuf},
+    stringify,
+};
 
 use detour::GenericDetour;
 use log::debug;
@@ -12,25 +17,36 @@ extern "stdcall" {
 }
 
 type FnCreateFileW = extern "stdcall" fn(*const u16, u32, u32, u64, u32, u32, u64) -> u64;
+type FnReadFile = extern "stdcall" fn(u64, *mut u8, u32, *mut u32, u64) -> u64;
 
 static mut SQPACK_REDIRECTOR: Option<SqPackRedirector> = None;
 
 pub struct SqPackRedirector {
     virtual_sqpack: VirtualSqPack,
     create_file_w: GenericDetour<FnCreateFileW>,
+    read_file: GenericDetour<FnReadFile>,
+}
+
+macro_rules! add_hook {
+    ($kernel32: expr, $target_fn: ty, $detour: expr) => {{
+        let hook_address = GetProcAddress($kernel32, stringify!($target_fn)[2..].as_ptr());
+        let hook = GenericDetour::<$target_fn>::new(std::mem::transmute(hook_address), $detour)?;
+        hook.enable()?;
+
+        Ok::<_, detour::Error>(hook)
+    }};
 }
 
 impl SqPackRedirector {
     pub unsafe fn start(virtual_sqpack: VirtualSqPack) -> detour::Result<()> {
         let kernel32 = GetModuleHandleW(WideCString::from_str("kernel32.dll").unwrap().as_ptr());
-        let create_file_w_address = GetProcAddress(kernel32, "CreateFileW".as_ptr());
-
-        let create_file_w = GenericDetour::<FnCreateFileW>::new(std::mem::transmute(create_file_w_address), Self::hooked_create_file_w)?;
-        create_file_w.enable()?;
+        let create_file_w = add_hook!(kernel32, FnCreateFileW, Self::hooked_create_file_w)?;
+        let read_file = add_hook!(kernel32, FnReadFile, Self::hooked_read_file)?;
 
         let redirector = Self {
             virtual_sqpack,
             create_file_w,
+            read_file,
         };
         SQPACK_REDIRECTOR.replace(redirector);
 
@@ -58,7 +74,6 @@ impl SqPackRedirector {
         if _self.virtual_sqpack.is_hooked_file(&path) {
             _self.create_virtual_file_handle(&path)
         } else {
-            #[allow(unused_unsafe)] // clippy bug?
             unsafe {
                 _self.create_file_w.call(
                     lp_file_name,
@@ -70,6 +85,22 @@ impl SqPackRedirector {
                     h_template_file,
                 )
             }
+        }
+    }
+
+    extern "stdcall" fn hooked_read_file(
+        h_file: u64,
+        lp_buffer: *mut u8,
+        n_number_of_byttes_to_read: u32,
+        lp_number_of_bytes_read: *mut u32,
+        lp_overlapped: u64,
+    ) -> u64 {
+        let _self = unsafe { SQPACK_REDIRECTOR.as_ref().unwrap() };
+
+        unsafe {
+            _self
+                .read_file
+                .call(h_file, lp_buffer, n_number_of_byttes_to_read, lp_number_of_bytes_read, lp_overlapped)
         }
     }
 }
