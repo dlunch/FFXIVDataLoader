59e287071501889f11eaf4acb2a0657673890f11
Inseok Lee <dlunch@gmail.com>
2020-09-24 20:30:16 +0900

    Use SqPackIndex type

--- src/virtual_sqpack/mod.rs
+++ src/virtual_sqpack/mod.rs
@@ -9,49 +9,61 @@ use log::debug;
 use pathdiff::diff_paths;
 use walkdir::WalkDir;
 
-use sqpack::{Result, SqPackArchiveId, SqPackFileReference, SqPackPackage};
+use sqpack::{internal::SqPackIndex, Result, SqPackArchiveId, SqPackFileReference, SqPackPackage};
 
-struct VirtualSqPackData {
-    header: Vec<u8>,
-    next_offset: u32,
+struct VirtualSqPackArchive {
+    index: SqPackIndex,
+    dat_header: Vec<u8>,
+    dat_index: u32,
+    next_dat_offset: u32,
     files: HashMap<u32, PathBuf>,
 }
 
-impl VirtualSqPackData {
-    pub async fn new(mut template_file: File) -> io::Result<Self> {
+impl VirtualSqPackArchive {
+    pub async fn new(index: SqPackIndex, mut template_file: File, dat_index: u32) -> io::Result<Self> {
         let mut header_template = vec![0; 0x800];
         template_file.read(&mut header_template).await?;
 
         Ok(Self {
-            header: header_template,
-            next_offset: 0,
+            index,
+            dat_header: header_template,
+            dat_index,
+            next_dat_offset: 0,
             files: HashMap::new(),
         })
     }
 
-    pub fn write(&mut self, path: &Path) -> u32 {
+    pub fn add_file(&mut self, file_path: &Path, archive_path: &str) -> Result<()> {
         let size_on_data = 1000; // TODO
 
-        let offset = self.next_offset;
-        self.next_offset += size_on_data;
-        self.files.insert(offset, path.into());
+        let offset = self.next_dat_offset;
+        self.next_dat_offset += size_on_data;
+        self.files.insert(offset, file_path.into());
 
-        offset
+        self.write_index(&SqPackFileReference::new(archive_path), offset)
     }
 
     pub fn read(&self, offset: u64, size: u64) -> &[u8] {
         if offset < 0x800 {
-            &self.header[offset as usize..offset as usize + size as usize]
+            &self.dat_header[offset as usize..offset as usize + size as usize]
         } else {
             panic!()
         }
+    }
+
+    fn write_index(&mut self, reference: &SqPackFileReference, new_offset: u32) -> Result<()> {
+        let new_offset = (self.dat_index << 1) | (new_offset >> 3);
+
+        self.index.write_offset(reference.hash.folder, reference.hash.file, new_offset)?;
+
+        Ok(())
     }
 }
 
 pub struct VirtualSqPack {
     sqpack_path: PathBuf,
     package: SqPackPackage,
-    data: HashMap<SqPackArchiveId, VirtualSqPackData>,
+    data: HashMap<SqPackArchiveId, VirtualSqPackArchive>,
 }
 
 impl VirtualSqPack {
@@ -77,6 +89,15 @@ impl VirtualSqPack {
     }
 
     pub fn is_hooked_file(&self, path: &Path) -> bool {
+        let relative_path = diff_paths(path, &self.sqpack_path);
+
+        if relative_path.is_some() {
+            let file_name = path.file_name().unwrap().to_str().unwrap();
+            let archive_id = SqPackArchiveId::from_sqpack_file_name(file_name);
+
+            return self.data.contains_key(&archive_id);
+        }
+
         false
     }
 
@@ -89,15 +110,9 @@ impl VirtualSqPack {
 
         let archive_id = SqPackArchiveId::from_file_path(archive_path);
         let archive = self.package.archive(archive_id).await?;
-        let mut archive = archive.write().await;
-
-        let reference = SqPackFileReference::new(archive_path);
 
         #[allow(clippy::map_entry)]
         if !self.data.contains_key(&archive_id) {
-            let new_dat_count = archive.index.dat_count() + 1;
-            archive.index.write_dat_count(new_dat_count);
-
             let data_file_name = format!("{:02x}{:02x}{:02x}.win32.dat0", archive_id.root, archive_id.ex, archive_id.part);
             let mut data_file_path = self.sqpack_path.clone();
             if archive_id.ex == 0 {
@@ -109,14 +124,19 @@ impl VirtualSqPack {
 
             debug!("Creating data file, template {:?}", data_file_path);
             let data_file = File::open(&data_file_path).await.unwrap();
-            self.data.insert(archive_id, VirtualSqPackData::new(data_file).await.unwrap());
+
+            let mut index_clone = archive.index.clone();
+            let new_dat_count = archive.index.dat_count() + 1;
+            index_clone.write_dat_count(new_dat_count);
+
+            self.data.insert(
+                archive_id,
+                VirtualSqPackArchive::new(index_clone, data_file, new_dat_count).await.unwrap(),
+            );
         }
 
-        let dat_index = archive.index.dat_count();
-        let dat_offset = self.data.get_mut(&archive_id).unwrap().write(file_path);
-        let new_offset = (dat_index << 1) | (dat_offset >> 3);
-
-        archive.index.write_offset(reference.hash.folder, reference.hash.file, new_offset)?;
+        let virtual_archive = self.data.get_mut(&archive_id).unwrap();
+        virtual_archive.add_file(file_path, archive_path)?;
 
         Ok(())
     }
