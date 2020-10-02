8eb247c1b96f8907f88ee1e7890f809f38ac23ba
Inseok Lee <dlunch@gmail.com>
2020-10-02 18:00:53 +0900

    Don't crash on different filename

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,7 @@ unsafe fn initialize() {
         // ffxiv_dx11.exe restarts itself with reduced privileges, making debugging harder.
         // but it doesn't restart if environment is darwin, so we mock darwin by writing flag value.
         let is_darwin_addr = 0x141B2F405u64 - 0x140000000; // for rev5516889
-        let ffxiv_base = crate::winapi::GetModuleHandleW(widestring::WideCString::from_str("ffxiv_dx11.exe").unwrap().as_ptr());
+        let ffxiv_base = crate::winapi::GetModuleHandleW(std::ptr::null());
         let is_darwin_ptr_addr = ffxiv_base + is_darwin_addr;
         let is_darwin_ptr: *mut u8 = std::mem::transmute(is_darwin_ptr_addr);
         *is_darwin_ptr = 1;
